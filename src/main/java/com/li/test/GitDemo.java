7f341f6347b7b6857b59d9552d43149da82a6b4e
li022721 <li022721@126.com>
2021-07-19 21:20:10 +0800

    Initial commit

---
+++ src/main/java/com/li/test/GitDemo.java
@@ -0,0 +1,11 @@
+package com.li.test;
+
+/**
+ * @author li
+ * @create 2021-07-19 21:19
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello github 111");
+    }
+}
